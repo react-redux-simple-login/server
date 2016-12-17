9c3828cb7386697955661ae07a3827c6b2e24d82
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-12-17 08:41:00 -0700

    login tests completed and failing

--- test/login.test.js
+++ test/login.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const expect = require('chai').expect;
 const request = require('supertest');
 const server = require('../server.js');
 
@@ -15,7 +14,7 @@ describe('POST /auth', () => {
       .expect(400, {err: 'bad request'}, done);
   });
 
-  it('throws an error if username does not match', (done) => {
+  xit('throws an error if username does not exist', (done) => {
     request(server)
       .post('/auth')
       .set('Accept', 'application/json')
@@ -24,8 +23,31 @@ describe('POST /auth', () => {
         password: 'password1'
       })
       .expect('Content-Type', /json/)
-      .expect(400, {err: 'bad request'}, done);
+      .expect(404, {err: 'user not found'}, done);
   });
 
+  xit('throws an error if username and password do not match', (done) => {
+    request(server)
+      .post('/auth')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'demo',
+        password: 'password513'
+      })
+      .expect('Content-Type', /json/)
+      .expect(404, {err: 'username and password do not match'}, done);
+  });
+
+  xit('responds with 200 and success message if username/password match', (done) => {
+    request(server)
+      .post('/auth')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'demo',
+        password: 'password1'
+      })
+      .expect('Content-Type', /json/)
+      .expect(200, {message: 'Login successful'}, done);
+  });
 
 });
