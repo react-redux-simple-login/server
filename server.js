8e3cfa6402da83cdafe47fc5f0c7fc42481335e6
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-12-17 08:15:10 -0700

    server.js setup complete

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const auth = require('./routes/auth.js');
+app.use('/auth', auth);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+module.exports = app;
